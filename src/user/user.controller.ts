efb57b92438973be34893de4f3e0d0c04ea0d9ab
Sergey Vergun <sewergun@gmail.com>
2024-03-10 23:20:59 +0300

    feat: add user service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,99 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
+import { User, UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+import { validate } from 'uuid';
+import { Response } from 'express';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getAll(): User[] {
+    return this.userService.getAll();
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: string, @Res() res: Response): User {
+    // TODO: Refactor repeated checks
+    if (!validate(id)) {
+      res.status(HttpStatus.BAD_REQUEST).send('Not valid user ID.');
+      return;
+    }
+
+    const user = this.userService.getById(id);
+
+    if (!user) {
+      res.status(HttpStatus.NOT_FOUND).send('User not found.');
+      return;
+    }
+
+    return user;
+  }
+
+  @Post()
+  createUser(@Body() dto: CreateUserDto, @Res() res: Response): User {
+    if (!dto.login || !dto.password) {
+      res
+        .status(HttpStatus.BAD_REQUEST)
+        .send('Required fields are not provided.');
+      return;
+    }
+
+    return this.userService.createUser(dto);
+  }
+
+  @Put(':id')
+  updateUser(
+    @Body() dto: UpdatePasswordDto,
+    @Param('id') id: string,
+    @Res() res: Response,
+  ): User {
+    if (!validate(id)) {
+      res.status(HttpStatus.BAD_REQUEST).send('Not valid user ID.');
+      return;
+    }
+
+    const user = this.userService.getById(id);
+    if (!user) {
+      res.status(HttpStatus.NOT_FOUND).send('User not found.');
+      return;
+    }
+
+    if (dto.oldPassword !== user.password) {
+      res.status(HttpStatus.FORBIDDEN).send('Wrong password.');
+      return;
+    }
+
+    return this.userService.updateUser(dto, id);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteUser(@Param('id') id: string, @Res() res: Response) {
+    if (!validate(id)) {
+      res.status(HttpStatus.BAD_REQUEST).send('Not valid user ID.');
+      return;
+    }
+
+    const user = this.userService.getById(id);
+    if (!user) {
+      res.status(HttpStatus.NOT_FOUND).send('User not found.');
+      return;
+    }
+
+    this.userService.deleteUser(id);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { v4 as uuidv4 } from 'uuid';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+
+export interface User {
+  id: string; // uuid v4
+  login: string;
+  password: string;
+  version: number; // integer number, increments on update
+  createdAt: number; // timestamp of creation
+  updatedAt: number; // timestamp of last update
+}
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private readonly users: Map<string, User> = new Map();
+
+  getAll(): User[] {
+    return Array.from(this.users.values());
+  }
+
+  getById(id: string): User {
+    return this.users.get(id);
+  }
+
+  createUser(dto: CreateUserDto): User {
+    const id = uuidv4();
+    const newUser: User = {
+      id,
+      ...dto,
+      version: 0,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+
+    this.users.set(id, newUser);
+
+    return newUser;
+  }
+
+  updateUser(dto: UpdatePasswordDto, id: string): User {
+    const user = this.getById(id);
+
+    user.password = dto.newPassword;
+    user.updatedAt = Date.now();
+    user.version++;
+
+    return user;
+  }
+
+  deleteUser(id: string) {
+    this.users.delete(id);
+  }
+}
