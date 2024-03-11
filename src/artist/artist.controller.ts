2d20f8c727619e705eccccb00b673d60c53aadb0
Sergey Vergun <sewergun@gmail.com>
2024-03-11 11:42:02 +0300

    feat: add artist logic

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,4 +1,94 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
+import { Artist, ArtistService } from './artist.service';
+import { validate } from 'uuid';
+import { Response } from 'express';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistDto } from './dto/update-artist.dto';
 
 @Controller('artist')
-export class ArtistController {}
+export class ArtistController {
+  constructor(private readonly artistService: ArtistService) {}
+
+  @Get()
+  getAll(): Artist[] {
+    return this.artistService.getAll();
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: string, @Res() res: Response): Artist {
+    // TODO: Refactor repeated checks
+    if (!validate(id)) {
+      res.status(HttpStatus.BAD_REQUEST).send('Not valid artist ID.');
+      return;
+    }
+
+    const artist = this.artistService.getById(id);
+
+    if (!artist) {
+      res.status(HttpStatus.NOT_FOUND).send('Artist not found.');
+      return;
+    }
+
+    return artist;
+  }
+
+  @Post()
+  createArtist(@Body() dto: CreateArtistDto, @Res() res: Response): Artist {
+    if (!dto.name || !dto.grammy) {
+      res
+        .status(HttpStatus.BAD_REQUEST)
+        .send('Required fields are not provided.');
+      return;
+    }
+
+    return this.artistService.createArtist(dto);
+  }
+
+  @Put(':id')
+  updateArtist(
+    @Body() dto: UpdateArtistDto,
+    @Param('id') id: string,
+    @Res() res: Response,
+  ): Artist {
+    if (!validate(id)) {
+      res.status(HttpStatus.BAD_REQUEST).send('Not valid artist ID.');
+      return;
+    }
+
+    const artist = this.artistService.getById(id);
+    if (!artist) {
+      res.status(HttpStatus.NOT_FOUND).send('Artist not found.');
+      return;
+    }
+
+    return this.artistService.updateArtist(dto, id);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteArtist(@Param('id') id: string, @Res() res: Response) {
+    if (!validate(id)) {
+      res.status(HttpStatus.BAD_REQUEST).send('Not valid artist ID.');
+      return;
+    }
+
+    const artist = this.artistService.getById(id);
+    if (!artist) {
+      res.status(HttpStatus.NOT_FOUND).send('Artist not found.');
+      return;
+    }
+
+    this.artistService.deleteArtist(id);
+  }
+}
---
+++ src/artist/artist.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistDto } from './dto/update-artist.dto';
+
+export interface Artist {
+  id: string; // uuid v4
+  name: string;
+  grammy: boolean;
+}
+
+@Injectable()
+export class ArtistService {
+  private readonly artists: Map<string, Artist> = new Map();
+
+  getAll(): Artist[] {
+    return Array.from(this.artists.values());
+  }
+
+  getById(id: string): Artist {
+    return this.artists.get(id);
+  }
+
+  createArtist(dto: CreateArtistDto): Artist {
+    const id = uuidv4();
+    const newArtist: Artist = {
+      id,
+      ...dto,
+    };
+
+    this.artists.set(id, newArtist);
+
+    return newArtist;
+  }
+
+  updateArtist(dto: UpdateArtistDto, id: string): Artist {
+    const artist = this.getById(id);
+
+    artist.name = dto.name;
+    artist.grammy = dto.grammy;
+
+    return artist;
+  }
+
+  deleteArtist(id: string) {
+    this.artists.delete(id);
+  }
+}
