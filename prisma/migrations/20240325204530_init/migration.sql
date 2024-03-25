c8a7c78b6bc0e77ce62358b677dd91e96143772c
Sergey Vergun <sewergun@gmail.com>
2024-03-25 23:46:26 +0300

    fix: update migration.sql

--- prisma/migrations/20240325204530_init/migration.sql
+++ prisma/migrations/20240325204530_init/migration.sql
@@ -15,7 +15,6 @@ CREATE TABLE "artists" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "grammy" BOOLEAN NOT NULL DEFAULT false,
-    "favoritesId" TEXT,
 
     CONSTRAINT "artists_pkey" PRIMARY KEY ("id")
 );
@@ -26,7 +25,6 @@ CREATE TABLE "albums" (
     "name" TEXT NOT NULL,
     "year" INTEGER NOT NULL,
     "artistId" TEXT,
-    "favoritesId" TEXT,
 
     CONSTRAINT "albums_pkey" PRIMARY KEY ("id")
 );
@@ -38,7 +36,6 @@ CREATE TABLE "tracks" (
     "artistId" TEXT,
     "albumId" TEXT,
     "duration" INTEGER NOT NULL,
-    "favoritesId" TEXT,
 
     CONSTRAINT "tracks_pkey" PRIMARY KEY ("id")
 );
@@ -46,6 +43,9 @@ CREATE TABLE "tracks" (
 -- CreateTable
 CREATE TABLE "favorites" (
     "id" TEXT NOT NULL,
+    "artists" TEXT[],
+    "albums" TEXT[],
+    "tracks" TEXT[],
 
     CONSTRAINT "favorites_pkey" PRIMARY KEY ("id")
 );
@@ -54,19 +54,10 @@ CREATE TABLE "favorites" (
 CREATE UNIQUE INDEX "users_login_key" ON "users"("login");
 
 -- AddForeignKey
-ALTER TABLE "artists" ADD CONSTRAINT "artists_favoritesId_fkey" FOREIGN KEY ("favoritesId") REFERENCES "favorites"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "albums" ADD CONSTRAINT "albums_artistId_fkey" FOREIGN KEY ("artistId") REFERENCES "artists"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "albums" ADD CONSTRAINT "albums_favoritesId_fkey" FOREIGN KEY ("favoritesId") REFERENCES "favorites"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "tracks" ADD CONSTRAINT "tracks_artistId_fkey" FOREIGN KEY ("artistId") REFERENCES "artists"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "tracks" ADD CONSTRAINT "tracks_albumId_fkey" FOREIGN KEY ("albumId") REFERENCES "albums"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "tracks" ADD CONSTRAINT "tracks_favoritesId_fkey" FOREIGN KEY ("favoritesId") REFERENCES "favorites"("id") ON DELETE SET NULL ON UPDATE CASCADE;
